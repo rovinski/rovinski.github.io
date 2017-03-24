3c544d9ab5bda1eedf81e3d86c4f877d38addfc3
Florian Klampfer <f.klampfer@gmail.com>
2017-03-24 12:09:53 +0100

    optimize

--- _js/src/flip/project.js
+++ _js/src/flip/project.js
@@ -12,10 +12,7 @@ import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/timer';
 
-import { animationFrame } from 'rxjs/scheduler/animationFrame';
-
 import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/observeOn';
 import 'rxjs/add/operator/zip';
 
 import { animate } from '../common';
@@ -23,59 +20,28 @@ import Flip from './flip';
 
 export default class ProjectFlip extends Flip {
   start(currentTarget) {
-    // FLIP
-    // Get the first position.
     const img = currentTarget.querySelector('.img');
-    const first = img.getBoundingClientRect();
-    // const x = currentTarget.querySelector('.name');
-    // const firstTitle = x.getBoundingClientRect();
-    // const firstFontSize = parseInt(getComputedStyle(x).fontSize, 10);
 
-    // const realImg = img.querySelector('img');
-    // realImg.src = realImg.currentSrc;
-    // realImg.removeAttribute('srcset');
-    // realImg.removeAttribute('sizes');
-
-    // Move it to the end.
-    // const imgEl = img.cloneNode(true);
-    img.classList.add('lead');
-    // img.removeAttribute('srcset');
-
-    img.style.transformOrigin = 'left top';
     this.shadowMain.querySelector('.page').innerHTML = `
       <h1 class="page-title" style="opacity:0">|</h1>
       <div class="post-date heading" style="opacity:0">|</div>
     `;
 
-    img.insertAdjacentHTML('beforebegin', '<div class="sixteen-nine"></div>');
+    const placeholder = document.createElement('div');
+    placeholder.classList.add('sixteen-nine');
+    img.parentNode.insertBefore(placeholder, img);
+    img.classList.add('lead');
+    img.style.transformOrigin = 'left top';
     this.shadowMain.querySelector('.page').appendChild(img);
     this.shadowMain.style.position = 'fixed';
     this.shadowMain.style.opacity = 1;
 
-    // Get the last position.
+    const first = placeholder.getBoundingClientRect();
     const last = img.getBoundingClientRect();
-    // const y = shadowMain.querySelector('.page-title');
-    // const lastTitle = y.getBoundingClientRect();
-    // const lastFontSize = parseInt(getComputedStyle(y).fontSize, 10);
 
-    // Invert.
     const invertX = first.left - last.left;
     const invertY = first.top - last.top;
     const invertScale = first.width / last.width;
-
-    // const titleInvertX = firstTitle.left - lastTitle.left;
-    // const titleInvertY = firstTitle.top - lastTitle.top;
-    // const titleInvertScale = firstFontSize / lastFontSize;
-
-    // animate(y, [
-    //   { transform:
-    //     `translate3d(${titleInvertX}px, ${titleInvertY}px, 0) scale(${titleInvertScale})` },
-    //   { transform: 'translate3d(0, 0, 0) scale(1)' },
-    // ], {
-    //   duration: this.duration,
-    //   easing: 'cubic-bezier(0,0,0.32,1)',
-    //   fill: 'forwards',
-    // }).subscribe();
 
     return animate(img, [
       { transform: `translate3d(${invertX}px, ${invertY}px, 0) scale(${invertScale})` },
@@ -83,33 +49,36 @@ export default class ProjectFlip extends Flip {
     ], {
       duration: this.duration,
       easing: 'cubic-bezier(0,0,0.32,1)',
-      // fill: 'forwards',
     })
       .do(() => { this.shadowMain.style.position = 'absolute'; });
   }
 
   ready(main) {
-    // main.querySelector(TITLE_SELECTOR).style.opacity = 0;
+    this.shadowMain.style.willChange = 'opacity';
 
-    // TODO: use id?
     const img = main.querySelector('.img');
-    img.style.opacity = 0;
+
+    if (img != null) {
+      img.style.opacity = 0;
+      img.style.willChange = 'opacity';
+    }
 
     const realImg = img.querySelector('img');
-
-    return (realImg != null ?
-      Observable.fromEvent(realImg, 'load') :
-      Observable.of(true)
+    return (realImg == null ?
+      Observable.of(true) :
+      Observable.fromEvent(realImg, 'load')
     )
-      .observeOn(animationFrame)
-      .zip(Observable.timer(this.duration + 100)) // HACK: add some extra time to preven hiccups
+      // HACK: add some extra time to prevent hiccups
+      .zip(Observable.timer(this.duration + 100))
       .do(() => {
-        img.style.opacity = 1;
-        this.shadowMain.style.opacity = 0;
-      });
-  }
+        if (img != null) {
+          img.style.opacity = 1;
+          img.style.willChange = '';
+        }
 
-  after() {
+        this.shadowMain.style.opacity = 0;
+        this.shadowMain.style.willChange = '';
+      });
   }
 }
 
