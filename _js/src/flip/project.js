7715f1af2c316951b9806d5cfd82a589a52755e0
Florian Klampfer <mail@qwtel.com>
2017-09-05 14:34:03 +0200

    dirty wip

--- _js/src/flip/project.js
+++ _js/src/flip/project.js
@@ -1,77 +1,91 @@
 // Copyright (c) 2017 Florian Klampfer <https://qwtel.com/>
 
 import { Observable } from 'rxjs/Observable';
+// import { Subject } from 'rxjs/Subject';
+
 import { of } from 'rxjs/observable/of';
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { timer } from 'rxjs/observable/timer';
+// import { timer } from 'rxjs/observable/timer';
 
 import { _do as effect } from 'rxjs/operator/do';
+import { filter } from 'rxjs/operator/filter';
+// import { mapTo } from 'rxjs/operator/mapTo';
+import { switchMap } from 'rxjs/operator/switchMap';
 // import { _finally as cleanup } from 'rxjs/operator/finally';
 import { zipProto as zipWith } from 'rxjs/operator/zip';
 
 import { animate } from '../common';
-import Flip from './flip';
 
-export default class ProjectFlip extends Flip {
-  start() {
-    const { anchor } = this;
-    const img = anchor.querySelector('.img');
+export function flipProject(start$, ready$, fadeIn$, { animationMain, duration }) {
+  const flip$ = start$
+    ::filter(({ flipType }) => flipType === 'project')
+    ::switchMap(({ anchor }) => {
+      console.log('project start');
+      const img = anchor.querySelector('.img');
 
-    const titleNode = anchor.parentNode.querySelector('.name') || {};
-    const title = titleNode.textContent || '|';
+      const titleNode = anchor.parentNode.querySelector('.name') || {};
+      const title = titleNode.textContent || '|';
 
-    this.animationMain.querySelector('.page').innerHTML = `
-      <h1 class="page-title" style="opacity:0">${title}</h1>
-      <div class="post-date heading" style="opacity:0">|</div>
-    `;
+      animationMain.querySelector('.page').innerHTML = `
+        <h1 class="page-title" style="opacity:0">${title}</h1>
+        <div class="post-date heading" style="opacity:0">|</div>
+      `;
 
-    const placeholder = document.createElement('div');
-    placeholder.classList.add('sixteen-nine');
+      const placeholder = document.createElement('div');
+      placeholder.classList.add('sixteen-nine');
 
-    img.parentNode.insertBefore(placeholder, img);
-    img.classList.add('lead');
-    img.style.transformOrigin = 'left top';
+      img.parentNode.insertBefore(placeholder, img);
+      img.classList.add('lead');
+      img.style.transformOrigin = 'left top';
 
-    this.animationMain.querySelector('.page').appendChild(img);
-    this.animationMain.style.position = 'fixed';
-    this.animationMain.style.opacity = 1;
+      animationMain.querySelector('.page').appendChild(img);
+      animationMain.style.position = 'fixed';
+      animationMain.style.opacity = 1;
 
-    const first = placeholder.getBoundingClientRect();
-    const last = img.getBoundingClientRect();
+      const first = placeholder.getBoundingClientRect();
+      const last = img.getBoundingClientRect();
 
-    const invertX = first.left - last.left;
-    const invertY = first.top - last.top;
-    const invertScale = first.width / last.width;
+      const invertX = first.left - last.left;
+      const invertY = first.top - last.top;
+      const invertScale = first.width / last.width;
 
-    return animate(img, [
-      { transform: `translate3d(${invertX}px, ${invertY}px, 0) scale(${invertScale})` },
-      { transform: 'translate3d(0, 0, 0) scale(1)' },
-    ], {
-      duration: this.duration,
-      easing: 'cubic-bezier(0,0,0.32,1)',
-    })
-      ::effect(() => { this.animationMain.style.position = 'absolute'; });
-  }
+      return animate(img, [
+        { transform: `translate3d(${invertX}px, ${invertY}px, 0) scale(${invertScale})` },
+        { transform: 'translate3d(0, 0, 0) scale(1)' },
+      ], {
+        duration,
+        easing: 'cubic-bezier(0,0,0.32,1)',
+      })
+        ::effect(() => { animationMain.style.position = 'absolute'; });
+    });
 
-  ready(main) {
-    this.animationMain.style.willChange = 'opacity';
+  start$::switchMap(() =>
+    ready$
+      ::filter(({ flipType }) => flipType === 'project')
+      ::switchMap(({ content: [main] }) => {
+        console.log('project ready');
+        animationMain.style.willChange = 'opacity';
 
-    const img = main.querySelector('.img');
-    const imgImg = img.querySelector('img');
+        const img = main.querySelector('.img');
+        const imgImg = img.querySelector('img');
 
-    img.style.opacity = 0;
-    img.style.willChange = 'opacity';
+        img.style.opacity = 0;
+        img.style.willChange = 'opacity';
 
-    return (imgImg == null ? Observable::of({}) : Observable::fromEvent(imgImg, 'load'))
-      /* HACK: add some extra time to prevent hiccups */
-      ::zipWith(Observable::timer(this.duration + 100))
-      ::effect(() => {
-        img.style.opacity = 1;
-        img.style.willChange = '';
-        this.animationMain.style.opacity = 0;
-        this.animationMain.style.willChange = '';
-      });
-  }
+        const img$ = imgImg == null ?
+          Observable::of() :
+          Observable::fromEvent(imgImg, 'load');
+
+        return img$
+          ::zipWith(fadeIn$)
+          ::effect(() => {
+            img.style.opacity = 1;
+            img.style.willChange = '';
+            animationMain.style.opacity = 0;
+            animationMain.style.willChange = '';
+          });
+      }))
+    .subscribe();
+
+  return flip$;
 }
-
-Flip.types.project = ProjectFlip;
