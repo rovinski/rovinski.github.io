edcafd5e6f7f7f09542d1263c25252d790bce94a
Florian Klampfer <mail@qwtel.com>
2018-07-02 08:25:29 +0700

    script wip

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+version=8.0.0-beta.2
+
 rm -rf _zip
 
-mkdir -p _zip/hydejack-pro-8.0.0-beta.1/.ssh
-cp ~/.ssh/hydejack_pro_customers _zip/hydejack-pro-8.0.0-beta.1/.ssh
+mkdir -p _zip/hydejack-pro-$version/.ssh
+cp ~/.ssh/hydejack_pro_customers _zip/hydejack-pro-$version.ssh
 
-mkdir -p _zip/hydejack-pro-8.0.0-beta.1/install
-mkdir -p _zip/hydejack-pro-8.0.0-beta.1/upgrade
+mkdir -p _zip/hydejack-pro-$version/install
+mkdir -p _zip/hydejack-pro-$version/upgrade
 
 # Make install folder
 cp -r \
@@ -22,7 +24,7 @@ cp -r \
     ! -name '_site*' \
     -mindepth 1 \
     -maxdepth 1) \
-  _zip/hydejack-pro-8.0.0-beta.1/install
+  _zip/hydejack-pro-$version/install
 
 # Make upgrade folder
 cp -r \
@@ -32,37 +34,37 @@ cp -r \
   assets \
   Gemfile* \
   package* \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade
+  _zip/hydejack-pro-$version/upgrade
 
 rm -r \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade/assets/icomoon \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade/assets/icons \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade/assets/img \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade/assets/ieconfig.xml \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade/assets/manifest.json \
-  _zip/hydejack-pro-8.0.0-beta.1/upgrade/assets/resume.json
+  _zip/hydejack-pro-$version/upgrade/assets/icomoon \
+  _zip/hydejack-pro-$version/upgrade/assets/icons \
+  _zip/hydejack-pro-$version/upgrade/assets/img \
+  _zip/hydejack-pro-$version/upgrade/assets/ieconfig.xml \
+  _zip/hydejack-pro-$version/upgrade/assets/manifest.json \
+  _zip/hydejack-pro-$version/upgrade/assets/resume.json
 
-find _zip/hydejack-pro-8.0.0-beta.1/upgrade/ -name 'my-*' -delete
-find _zip/hydejack-pro-8.0.0-beta.1/ -name '.DS_Store' -delete
+find _zip/hydejack-pro-$version/upgrade/ -name 'my-*' -delete
+find _zip/hydejack-pro-$version/ -name '.DS_Store' -delete
 
 # Generate PDFs.
 # This assumes the next version is already online at qwtel.com
 # This also assumes macOS with chrome installed...
 function pdfprint {
-  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf="_zip/hydejack-pro-8.0.0-beta.1/$1.pdf" $2
+  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf="_zip/hydejack-pro-$version/$1.pdf" $2
 }
 
 pdfprint "PRO License" "https://hydejack.com/licenses/PRO/"
 pdfprint "PRO–hy-drawer License" "https://qwtel.com/hy-drawer/licenses/hydejack/"
 pdfprint "PRO–hy-push-state License" "https://qwtel.com/hy-push-state/licenses/hydejack/"
 pdfprint "PRO–hy-img License" "https://qwtel.com/hy-img/licenses/hydejack/"
-pdfprint "Documentation" "https://hydejack.com/docs/8.0.0-beta.1/print/"
+pdfprint "Documentation" "https://hydejack.com/docs/$version/print/"
 pdfprint "NOTICE" "https://hydejack.com/NOTICE/"
 pdfprint "CHANGELOG" "https://hydejack.com/CHANGELOG/"
 
 # Genrate git diffs
 # TODO
-# git diff pro/v8.0.0-beta.1 pro/v7.5.1 > v8.0.0-beta.1-to-v7.5.1.diff
+# git diff pro/v$version pro/v7.5.1 > v$version-to-v7.5.1.diff
 
 # Generate the zip
-cd _zip; zip -q -r hydejack-pro-8.0.0-beta.1.zip hydejack-pro-8.0.0-beta.1/
+cd _zip; zip -q -r hydejack-pro-$version.zip hydejack-pro-$version/
